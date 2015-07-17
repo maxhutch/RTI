95a648e8d0b6706633e714e42756dfe7abfaba2c
Max Hutchinson <maxhutch@gmail.com>
2015-07-17 14:17:40 +0000

    Turned off filtering, expanded z, fixed-time outputs.

--- HighAspect/HA.swift
+++ HighAspect/HA.swift
@@ -5,26 +5,27 @@ type file;
 string prefix="HA";
 file json <"HA.json">;
 file tusr <"single_mode.tusr">;
-//file tusr <"Hill_f77.tusr">;
-//string pname="tolrel";
-string pname="conductivity";
-float[] pvals = [0.00001];
-//float[] pvals=[0.001, 0.002, 0.003, 0.004, 0.005, 0.006];
+
+string pname="viscosity";
+//float[] pvals = [0.0002, 0.0001, 0.00005];
+//float[] pvals = [0.0004, 0.0002, 0.0001];
+float[] pvals = [0.0004];
+
 int nwrite=256;
 boolean legacy = false;
 
-/*
-int nstep = 16384;
-int io_step = 128;
-int step_block = 1024;
-int foo = 8;
-*/
 int nodes = 1024;
-int mode = 64;
-int nstep = 512;
-int io_step = 128;
-int step_block = 512;
+int mode = 32;
 int jtime = 60;
 
+int nstep = 60;
+//int io_step = 128;
+int io_step = 1;
+//int step_block = 512;
+int step_block = 2;
+
+float io_time = 2.0;
+float time_block = 4.0;
+
 int test;
-test = sweep(prefix, json, tusr, pname, pvals, nwrite, legacy, nstep, io_step, step_block, nodes, mode, jtime);
+test = sweep(prefix, json, tusr, pname, pvals, nwrite, legacy, nstep, io_step, step_block, io_time, time_block, nodes, mode, jtime);
